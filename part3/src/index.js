a0d2534e2df7614b21945272afd655e9ed9401d7
2020jun <1160364324@qq.com>
2020-07-13 20:35:06 +0800

    简单计数器

---
+++ part3/src/index.js
@@ -0,0 +1,80 @@
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom'
+
+/*const Hello = ({name, age }) => {
+  
+  
+  const bornYear = () =>  new Date().getFullYear() - age
+  
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+     
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
+*/
+
+const Display= ({counter}) => <div>{counter} </div>
+   
+
+const Button = ({handleClick,text}) =>{
+  return(
+    <button onClick = {handleClick}>
+        {text}
+    </button>
+  )
+}
+
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+
+ /* const handleClick = () =>{
+    console.log('click')
+  }*/
+  return (
+    <div>
+      <Display counter = {counter} />
+      <Button   handleClick = {increaseByOne}
+      text = 'push'
+      />
+      <Button handleClick = {decreaseByOne}
+      text = 'minus'
+      />
+      <Button handleClick ={setToZero}
+      text = 'zero'
+      />
+    </div>
+ 
+
+  )
+}
+
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
