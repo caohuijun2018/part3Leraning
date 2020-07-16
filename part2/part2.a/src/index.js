63cadeb14870cc3cf9c329e24433a0760a5d25a7
2020jun <1160364324@qq.com>
2020-07-16 22:13:40 +0800

    独立模块

--- part2/part2.a/src/index.js
+++ part2/part2.a/src/index.js
@@ -1,38 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const Courses = ({ courses }) => {
- 
- const reducer = (accumulator, currentValue) => accumulator + currentValue;
- const total1 = courses[0].parts.map(note => note.exercises).reduce(reducer)
- const total2 = courses[1].parts.map(note => note.exercises).reduce(reducer)
-  return (
-    <div>
-      <h1> {courses[0].name}</h1>
-      <ul>
-        {courses[0].parts.map(note =>
-          <p key = {note.id}>{note.name} {note.exercises}</p>
-          )}
-          <p>total of {total1} exercises</p>
-      </ul>
-      <h1> {courses[1].name}</h1>
-      <ul>
-        {courses[1].parts.map(note =>
-          <p key = {note.id}>{note.name} {note.exercises}</p>
-          )}
-          <p>total of {total2} exercises</p>
-      </ul>
-      {/* <ul> */}
-        
-        {/* {course.parts.map(note => */}
-          {/* <p key={note.id}> {note.name} {note.exercises}</p> */}
-        {/* )} */}
-         {/* {course.parts.forEach(note => { total += note.exercises }  */}
-        
-        {/* <p> total of {total} exercises</p> */}
-      {/* </ul> */}
-    </div>
-  )
-}
+import Courses from './components/Courses'
+
 //const App = () => {
   /*const course = {
     id: 1,
