e84426ee2c0844093d40e341b0ffeea340869a3a
2020jun <1160364324@qq.com>
2020-07-16 21:56:13 +0800

    增加courses

--- part2/part2.a/src/index.js
+++ part2/part2.a/src/index.js
@@ -1,25 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const Course = ({ course }) => {
- //let total = 0;
+const Courses = ({ courses }) => {
+ 
  const reducer = (accumulator, currentValue) => accumulator + currentValue;
- const total = course.parts.map(note => note.exercises).reduce(reducer)
+ const total1 = courses[0].parts.map(note => note.exercises).reduce(reducer)
+ const total2 = courses[1].parts.map(note => note.exercises).reduce(reducer)
   return (
     <div>
-      <h1> {course.name}</h1>
+      <h1> {courses[0].name}</h1>
       <ul>
-        {course.parts.map(note =>
-          <p key={note.id}> {note.name} {note.exercises}</p>
-        )}
+        {courses[0].parts.map(note =>
+          <p key = {note.id}>{note.name} {note.exercises}</p>
+          )}
+          <p>total of {total1} exercises</p>
+      </ul>
+      <h1> {courses[1].name}</h1>
+      <ul>
+        {courses[1].parts.map(note =>
+          <p key = {note.id}>{note.name} {note.exercises}</p>
+          )}
+          <p>total of {total2} exercises</p>
+      </ul>
+      {/* <ul> */}
+        
+        {/* {course.parts.map(note => */}
+          {/* <p key={note.id}> {note.name} {note.exercises}</p> */}
+        {/* )} */}
          {/* {course.parts.forEach(note => { total += note.exercises }  */}
         
-        <p> total of {total} exercises</p>
-      </ul>
+        {/* <p> total of {total} exercises</p> */}
+      {/* </ul> */}
     </div>
   )
 }
-const App = () => {
-  const course = {
+//const App = () => {
+  /*const course = {
     id: 1,
     name: 'Half Stack application development',
     parts: [
@@ -44,9 +59,54 @@ const App = () => {
         id: 4
       }
     ]
-  }
+  }*/
+  const App = () => {
+    const courses = [
+      {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
   
-  return <Course course={course} />
+  return <Courses courses={courses} />
 
 }
 ReactDOM.render(
