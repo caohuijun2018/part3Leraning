ca4e3129b4db2fc4859ac81b77e85e69285ae80b
2020jun <1160364324@qq.com>
2020-07-19 13:22:08 +0800

    增加姓名和电话的功能

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      id: 1,
+      phone: 15612345678
+    }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const add = (event) => {
+    event.preventDefault()
+    const Object = {
+      name: newName,
+      id: persons.length + 1,
+      phone: newPhone
+    }
+    //console.log(Objext.content)
+    setPersons(persons.concat(Object))
+    setNewName('')
+    setNewPhone('')
+  }
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+  persons.forEach(props => {
+    console.log(props.name)
+    if (props.name === newName) {
+      window.alert(`${newName} is already added to phonebook`)
+
+    }
+
+  }
+  )
+  /*let isinclude = persons.includes(newName)
+  console.log(isinclude)
+  if (isinclude === true ) window.alert(`${newName} is already added to phonebook`)*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={add}>
+        <div>
+          name:
+          <input value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          phone:
+          <input value={newPhone}
+            onChange={handlePhoneChange} />
+        </div>
+
+        <h2>Numbers</h2>
+
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <form>
+     
+        {persons && persons.map(note =>
+          <li key={note.id}>   {note.name} {note.phone}</li>
+        )}
+        {/* {persons && persons.map(note =>
+          <li key={note.id}> {note.phone} </li>
+        )} */}
+        
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
