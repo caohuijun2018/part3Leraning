005842813feefc0d39dff9bceab4464f7629236f
2020jun <1160364324@qq.com>
2020-07-19 20:42:51 +0800

    创建筛选

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react'
 //import logo from './logo.svg';
 import './App.css';
-
-
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      id: 1,
-      phone: 15612345678
-    }
+    { name: 'Arto Hellas', id: 1, phone: '1561234567' },
+    { name: 'Ada Lovelace', id: 2, phone: '39-44-5323523' },
+    { name: 'Dan Abramov', id: 3, phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', id: 4, phone: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterName, setFNewilterName] = useState('')
   const add = (event) => {
     event.preventDefault()
     const Object = {
@@ -20,66 +18,67 @@ const App = () => {
       id: persons.length + 1,
       phone: newPhone
     }
-    //console.log(Objext.content)
-    setPersons(persons.concat(Object))
-    setNewName('')
-    setNewPhone('')
+    let flag = 0;
+    persons.forEach(props => {
+      if (props.name === newName) {
+        window.alert(`${newName} is already added to phonebook`)
+        flag = 1;
+      }
+    })
+    if (flag === 0) {
+      setPersons(persons.concat(Object))
+      setNewName('')
+      setNewPhone('')
+    }
+    setFNewilterName(filterName.concat(filterName))
+
   }
   const handleNameChange = (event) => {
-    //console.log(event.target.value)
     setNewName(event.target.value)
   }
   const handlePhoneChange = (event) => {
     setNewPhone(event.target.value)
   }
-  persons.forEach(props => {
-    console.log(props.name)
-    if (props.name === newName) {
-      window.alert(`${newName} is already added to phonebook`)
-
-    }
-
+  const handleFilterName = (event) => {
+    setFNewilterName(event.target.value)
   }
-  )
-  /*let isinclude = persons.includes(newName)
-  console.log(isinclude)
-  if (isinclude === true ) window.alert(`${newName} is already added to phonebook`)*/
-
   return (
     <div>
-      <h2>Phonebook</h2>
-
-      <form onSubmit={add}>
+      <div>
+        <h2>phonebook</h2>
         <div>
-          name:
-          <input value={newName}
-            onChange={handleNameChange}
+          filter shown with
+          <input value={filterName}
+            onChange={handleFilterName}
           />
         </div>
-        <div>
-          phone:
-          <input value={newPhone}
-            onChange={handlePhoneChange} />
-        </div>
-
-        <h2>Numbers</h2>
-
-
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      </div>
+      <h2>Add a new </h2>
+     < form onSubmit={add}>
+    <div>
+      name:
+    <input value={newName}
+        onChange={handleNameChange}
+      />
+    </div>
+    <div>
+      phone:
+    <input value={newPhone}
+        onChange={handlePhoneChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+      {/* <Peopleadd add={add} newName={newName} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange} /> */}
       <form>
-     
-        {persons && persons.map(note =>
-          <li key={note.id}>   {note.name} {note.phone}</li>
+        <h2>Numbers</h2>
+        {persons && persons.filter(note => note.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1).map(note =>
+          <p key={note.id}>  {note.name} {note.phone}</p>
         )}
-        {/* {persons && persons.map(note =>
-          <li key={note.id}> {note.phone} </li>
-        )} */}
-        
       </form>
     </div>
+
   )
 }
 
