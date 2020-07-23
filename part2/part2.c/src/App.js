19503ee708fa91fd4b1e70901078a6b76ed35bc7
2020jun <1160364324@qq.com>
2020-07-23 16:21:47 +0800

    完善便签

--- part2/part2.c/src/App.js
+++ part2/part2.c/src/App.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
-  const [notes, setNotes] = useState([]) 
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes  => {
+        setNotes(initialNotes )
       })
+
+    // axios
+    //   .get('http://localhost:3001/notes')
+    //   .then(response => {
+    //     console.log('promise fulfilled')
+    //     setNotes(response.data)
+    //   })
   }, [])
 
-  console.log('render', notes.length, 'notes')
+  //console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -26,16 +32,22 @@ const App = () => {
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
       //id: notes.length + 1,
-}
-axios
-    .post('  http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
-      setNewNote('')
-    })
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    }
+    noteService
+      .create(noteObject)
+      .then(returnedNote  => {
+        setNotes(notes.concat(returnedNote ))
+        setNotes('')
+      })
+    // axios
+    //     .post('  http://localhost:3001/notes', noteObject)
+    //     .then(response => {
+    //       setNotes(notes.concat(response.data))
+    //       setNewNote('')
+    //     })
+
+    //     setNotes(notes.concat(noteObject))
+    //     setNewNote('')
   }
 
   const handleNoteChange = (event) => {
@@ -46,19 +58,39 @@ axios
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
-
+  const toggleImportanceOf = id => {
+    // const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote  => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote ))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+    // axios.put(url, changedNote).then(response => {
+    //   setNotes(notes.map(note => note.id !== id ? note : response.data))
+    // })
+  }
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
-      </div>      
+      </div>
       <ul>
-        {notesToShow.map((note, i) => 
-          <Note key={i} note = {note} />
-        )} 
+        {notesToShow.map((note, i) =>
+          <Note key={i} note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
       </ul>
       <form onSubmit={addNote}>
         <input
@@ -66,7 +98,7 @@ axios
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>   
+      </form>
     </div>
   )
 }
--- part2/part2.c/src/components/Note.js
+++ part2/part2.c/src/components/Note.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Note = ({ note }) => {
+const Note = ({ note,toggleImportance }) => {
+  const lable = note.important
+  ? 'make not important' : 'make important'
   return (
-    <li>{note.content}</li>
+    <li>
+      {note.content}
+  <button onClick = {toggleImportance}>{lable}</button>
+    </li>
   )
 }
 
 export default Note
+
+
