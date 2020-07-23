508d9ee0b520f201d583c6a84ae24e93e661f1c4
2020jun <1160364324@qq.com>
2020-07-23 18:59:21 +0800

    创建内容

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 //import logo from './logo.svg';
 import './App.css';
-import _ from 'lodash'
+//import _ from 'lodash'
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1, phone: '1561234567' },
-    { name: 'Ada Lovelace', id: 2, phone: '39-44-5323523' },
-    { name: 'Dan Abramov', id: 3, phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', id: 4, phone: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filterName, setFNewilterName] = useState('')
+  // useEffect(() => {
+  //   axios
+  //     .get('http://localhost:3001/persons')
+  //     .then(response => {
+  //       console.log('promise fulfilled')
+  //       setPersons(response.data)
+  //     })
+  // }, [])
+
+
+  
   const add = (event) => {
     event.preventDefault()
     const Object = {
@@ -72,9 +80,7 @@ const App = () => {
     <div>
       <button type="submit">add</button>
     </div>
-  </form>
-      {/* <Peopleadd add={add} newName={newName} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange} /> */}
-      <form>
+
         <h2>Numbers</h2>
         {persons && persons.filter(note => note.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1).map  (note =><p key={note.id}>  {note.name} {note.phone}</p>)
 
