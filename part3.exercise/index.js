f4af35f4f6ca22fb3f4c4b844a281de66f9a5c17
caohuijun2018 <1160364324@qq.com>
2020-07-28 21:32:49 +0800

    实现后端的读取，增删

---
+++ part3.exercise/index.js
@@ -0,0 +1,101 @@
+const http = require('http')
+
+const express = require('express')
+const { response, request } = require('express')
+const app = express()
+const morgan = express('morgan')
+app.use(express.json())
+
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "034-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelac",
+        number: "39-44-5325525",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234325",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    }
+]
+
+
+app.get('/api/persons', (requese, response) => {
+    response.json(persons)
+})
+app.get('/info', (request, response) => {
+    let numberPersons = persons.length;
+    let time = new Date()
+    response.send(`<p>Phonebook has info to ${numberPersons} people</p>
+
+    <p>the time is ${time}</p>`
+    )
+})
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+
+    response.status(204).end()
+})
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+  })
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    console.log(body)
+    if (!body.name ) {
+        return response.status(404).json(
+            { error: 'name must be uniqe' }
+        )
+    }
+    if(!body.number){
+        return response.status(404).json({
+            error: "phoneNumber must be uniqe"
+        })
+    }
+    persons.forEach(person => {
+        if(person.name === body.name || person.number === body.number){
+            return response.status(404).json({
+                erroe: "this name already exists"
+            })
+        }
+    })
+    const person = {
+        name: body.name,
+        number: body.number,
+        id:  Math.floor(  Math.random() * 1000)
+    }
+    
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+  
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
